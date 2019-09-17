0b3658740320230d89a5450538adbaa6c6f47f18
j1xaro <konecnik.jan@gmail.com>
2019-09-17 13:31:19 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-include_once './header.php';
+//include_once './header.php';
 ?>
 
 <!DOCTYPE html>
@@ -26,13 +26,55 @@ include_once './header.php';
   <link href="css/grayscale.min.css" rel="stylesheet">
 
 </head>
-<h1>Prijava</h1>
 
-<form action="login_check.php" method="post">
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="index.php">PROrum</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Meni
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#projects">Forum</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#projects">Pravila</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="odjava.php">Registracija</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Header -->
+  <header class="masthead">
+    <div class="container d-flex h-100 align-items-center">
+      <div class="mx-auto text-center">
+        <h2 class="mx-auto my-0 text-uppercase">
+          <?php
+          echo "Prijava";
+          
+          ?>
+        </h2>
+        <h2 class="text-white-50 mx-auto mt-2 mb-5">
+            <form action="login_check.php" method="post">
     <input type="email" name="email" class="" placeholder="Vnesite e-pošto" required="required" />
     <input type="password" name="pass" class="" placeholder="Vnesite geslo" required="required" />
     <input type="submit" class="" value="Prijava" />
 </form>
+</h2>
+<h2 class="text-white-50 mx-auto mt-2 mb-5">Če še niste registrirani, se lahko s pritiskom na spodenj gumb.</h2>+
+        <a href="register.php" class="btn btn-primary js-scroll-trigger">Registracija</a>
+      </div>
+    </div>
+  </header>
 
 <?php
 include_once './footer.php';
