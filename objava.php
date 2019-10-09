90b8c91373b4f2c9f37b29256bfa2c70aa2be9a0
j1xaro <konecnik.jan@gmail.com>
2019-10-09 08:53:20 +0200

    Update objava.php
    uredil stran objava da pokaže objavo

---
+++ objava.php
@@ -0,0 +1,361 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+session_start();
+    require_once "database.php";  
+if (isset($_SESSION['id'])){
+    $objava = $_GET['obj'];
+    $query1 = "SELECT * FROM objave where id=".$objava;
+    $stmt1 = $pdo->prepare($query1);
+    $stmt1->execute();
+    $row1 = $stmt1->fetch();
+
+    $query2 = "SELECT * FROM uporabniki where id=". $row1['id_uporabnik'];
+    $stmt2 = $pdo->prepare($query2);
+    $stmt2->execute();
+    $row2 = $stmt2->fetch();
+
+    $query = "SELECT * FROM uporabniki where id = ".$_SESSION['id'];
+    $stmt = $pdo->prepare($query);
+    $stmt->execute();
+    $row = $stmt->fetch();
+    if ($_SESSION['admin'] == "admin"){ ?>
+    
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Domov</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/grayscale.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">PROrum</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Meni
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="index.php">Domov</a>
+          </li>
+        <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="pravila.php">Pravila</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="admin.php">Admin</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="odjava.php">Odjava</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+ 
+  <header class="masthead">
+    <div class="container d-flex h-100 align-items-center">
+      <div class="mx-auto text-center">
+        <h2 class="mx-auto my-0 text-uppercase">
+        
+        </h2>
+        <h2 class="text-white-50 mx-auto mt-2 mb-5">
+        
+        <?php 
+          echo "<label><b>Naslov:</b></label><h3>".  $row1['naslov_objave'] . "</h3></a><label><b>Objavljeno:</b></label><h5> " . $row1['datum_objave']. "</h5><label><b>Objavil:</b></label><h5>". $row2['email']. "</h5><label><b>Text:</b></label><h4>". $row1['text']."</h4>";
+        ?>
+        
+        </h2>
+        
+      </div>
+    </div>
+  </header>
+
+  
+  <!-- Contact Section -->
+  <section class="contact-section bg-black">
+    <div class="container">
+
+      <div class="social d-flex justify-content-center">
+        <a href="https://twitter.com/jkxycs" target="_blank" class="mx-2">
+          <i class="fab fa-twitter"></i>
+        </a>
+       <?php /* <a href="#" class="mx-2">
+          <i class="fab fa-facebook-f"></i>
+        </a> */ ?>
+        <a href="https://github.com/j1xaro" target="_blank" class="mx-2">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="bg-black small text-center text-white-50">
+    <div class="container">
+      Copyright &copy; Your Website 2019
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/grayscale.min.js"></script>
+
+</body>
+
+</html>
+
+    <?php } 
+    else { 
+        ?>
+
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>Domov</title>
+
+<!-- Bootstrap core CSS -->
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom fonts for this template -->
+<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="css/grayscale.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+  <div class="container">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">PROrum</a>
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      Meni
+      <i class="fas fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="index.php">Domov</a>
+        </li>
+      <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="pravila.php">Pravila</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="nastavitve.php">Nastavitve</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="odjava.php">Odjava</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<header class="masthead">
+    <div class="container d-flex h-100 align-items-center">
+      <div class="mx-auto text-center">
+        <h2 class="mx-auto my-0 text-uppercase">
+        <strong>Teme</strong>
+        </h2>
+        <h2 class="text-white-50 mx-auto mt-2 mb-5">
+        <?php 
+          echo "<label><b>Naslov:</b></label><h3>".  $row1['naslov_objave'] . "</h3></a><label><b>Objavljeno:</b></label><h5> " . $row1['datum_objave']. "</h5><label><b>Objavil:</b></label><h5>". $row2['email']. "</h5><label><b>Text:</b></label><h4>". $row1['text']."</h4>";
+
+        ?>
+        </h2>
+        
+      </div>
+    </div>
+  </header>
+
+
+<!-- Contact Section -->
+<section class="contact-section bg-black">
+  <div class="container">
+
+    <div class="social d-flex justify-content-center">
+      <a href="https://twitter.com/jkxycs" target="_blank" class="mx-2">
+        <i class="fab fa-twitter"></i>
+      </a>
+     <?php /* <a href="#" class="mx-2">
+        <i class="fab fa-facebook-f"></i>
+      </a> */ ?>
+      <a href="https://github.com/j1xaro" target="_blank" class="mx-2">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+
+  </div>
+</section>
+
+<!-- Footer -->
+<footer class="bg-black small text-center text-white-50">
+  <div class="container">
+    Copyright &copy; Your Website 2019
+  </div>
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for this template -->
+<script src="js/grayscale.min.js"></script>
+
+</body>
+
+</html>
+
+    <?php } }
+
+else { ?>
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Domov</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/grayscale.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">PROrum</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Meni
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="index.php">Domov</a>
+          </li>
+        <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="pravila.php">Pravila</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="login.php">Prijava</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="register.php">Registracija</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+ 
+  <header class="masthead">
+    <div class="container d-flex h-100 align-items-center">
+      <div class="mx-auto text-center">
+        <h2 class="mx-auto my-0 text-uppercase">
+        <strong>Teme</strong>
+        </h2>
+        <h2 class="text-white-50 mx-auto mt-2 mb-5">
+        <?php 
+          echo "<label><b>Naslov:</b></label><h3>".  $row1['naslov_objave'] . "</h3></a><label><b>Objavljeno:</b></label><h5> " . $row1['datum_objave']. "</h5><label><b>Objavil:</b></label><h5>". $row2['email']. "</h5><label><b>Text:</b></label><h4>". $row1['text']."</h4>";
+        ?>
+        </h2>
+        
+      </div>
+    </div>
+  </header>
+
+  
+  <!-- Contact Section -->
+  <section class="contact-section bg-black">
+    <div class="container">
+
+      <div class="social d-flex justify-content-center">
+        <a href="https://twitter.com/jkxycs" target="_blank" class="mx-2">
+          <i class="fab fa-twitter"></i>
+        </a>
+       <?php /* <a href="#" class="mx-2">
+          <i class="fab fa-facebook-f"></i>
+        </a> */ ?>
+        <a href="https://github.com/j1xaro" target="_blank" class="mx-2">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="bg-black small text-center text-white-50">
+    <div class="container">
+      Copyright &copy; Your Website 2019
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/grayscale.min.js"></script>
+
+</body>
+
+</html>
+
+<?php }
+?>
